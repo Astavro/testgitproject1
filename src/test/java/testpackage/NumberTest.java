f1a04a8d1b30ece4469166ceda0a4dadf3d63519
Tony <tony@git>
2023-11-14 20:11:07 -0500

    number test

---
+++ src/test/java/testpackage/NumberTest.java
@@ -0,0 +1,15 @@
+package testpackage;
+
+import java.util.Iterator;
+
+public class NumberTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for (int i =0; i<13; i++) {
+			System.out.println(i);
+			
+		}
+	}
+
+}
