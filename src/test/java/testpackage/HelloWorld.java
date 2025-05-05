eb11eb78f511ee5111b0d3c70dc4c2779d615876
Antoine_1 <antoinovich@hotmail.com>
2025-05-04 23:02:36 -0400

    added loop

--- src/test/java/testpackage/HelloWorld.java
+++ src/test/java/testpackage/HelloWorld.java
@@ -3,8 +3,11 @@ package testpackage;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		System.out.println("Hello");
+		
+		for(int i=0; i<5; i++) {
+			System.out.println("Hello:  " + i);
+		}
 	}
 
 }
