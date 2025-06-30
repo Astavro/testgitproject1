5b53c1c7f47e918df9045187dec1cb3bfb8e4b0b
Antoine <antoinovich@hotmail.com>
2025-06-30 00:15:20 -0400

    added concat function

--- src/test/java/testpackage/HelloWorld.java
+++ src/test/java/testpackage/HelloWorld.java
@@ -6,6 +6,8 @@ public class HelloWorld {
 		
 		System.out.println(myFunc(0));
 		System.out.println(myFunc(1));
+		
+		System.out.println(concat("Go ", "Antoine"));
 	}
 
 	public static String myFunc(int x) {
@@ -15,5 +17,9 @@ public class HelloWorld {
 			return "Yes";
 		else
 			return "No";
+	}
+	
+	public static String concat(String str1, String str2) {
+		return str1 + str2;
 	}
 }
